335bebb99430a15fb2ff8f5262b18eb4a525a8c6
Ramon Diego dos Santos Ferreira <ramondiego857@gmail.com>
2021-12-29 22:56:39 -0300

    Mais tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,4 +1,21 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+
+        int age = 18;
+        long numeroGrande = 10000000;
+        double salario = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        System.out.println("A idade é:"+ age + " anos");
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
+    }
 }
