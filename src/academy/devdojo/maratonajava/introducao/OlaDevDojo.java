c5f3e01539286f3318251be22928d914238306dd
Ramon Diego dos Santos Ferreira <ramondiego857@gmail.com>
2021-12-29 21:53:43 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OlaDevDojo {
+//    Isso é um comentário
+
+    /*
+    Esse é um
+    comentário de
+    multiplas linhas
+   */
+
+    public static void main(String[] args) {
+        System.out.println("Ódio");
+    }
+}
